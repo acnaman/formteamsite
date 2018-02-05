9484484aa7a79170d0e657ef34b7ad0bd4d962c7
acnaman <0B2012@prodgrp.iftc.co.jp>
2018-02-05 18:04:11 +0900

    add index

--- kaizen/models.py
+++ kaizen/models.py
@@ -2,53 +2,48 @@ from django.db import models
 
 # Create your models here.
 
-
+# メンバー管理クラス
 class Member(models.Model):
-    #id = models.IntegerField(unique=True)
     name = models.CharField(max_length=30)
 
+# KeepとProblem用クラス
 class ProblemKeep(models.Model):
-    #id = models.CharField(unique=True, max_length=5)
     start_date = models.DateField()
     title = models.CharField(max_length=200)
-    presenter = models.ForeignKey(Member, related_name='pk_presenter')#IntegerField()
-    contributer = models.ManyToManyField(Member, related_name='pk_contributer', blank=True)#(blank=True, null=True)
+    presenter = models.ForeignKey(Member, related_name='pk_presenter')
+    contributer = models.ManyToManyField(Member, related_name='pk_contributer', blank=True)
 
+# Tryの親要素
 class TryParent(models.Model):
-    #id = models.CharField(unique=True, max_length=5)
     target_kp = models.ForeignKey(ProblemKeep)
     try_idea = models.CharField(max_length=200)
 
+# 実際に行ったTry
 class TryAction(models.Model):
-    #id = models.CharField(unique=True, max_length=5)
     try_action = models.CharField(max_length=200)
     actor = models.ManyToManyField(Member, blank=True)
     canceled_date = models.DateField(blank=True, null=True)
     canceled_reason = models.CharField(max_length=200, blank=True, null=True)
     try_idea = models.ForeignKey(TryParent, blank=True, null=True)
 
+# Tryに対するコメント  
 class Comment(models.Model):
-    #id = models.IntegerField()
     comment = models.CharField(max_length=500, blank=True, null=True)
     belong = models.ForeignKey(TryAction, blank=True, null=True)
 
-
-
+# レビュークラス
 class Review(models.Model):
-    #id = models.IntegerField(unique=True)
     schedule = models.DateField(blank=True, null=True)
     result = models.CharField(max_length=200, blank=True, null=True)
     meeting = models.ForeignKey(TryAction, blank=True, null=True)
 
-
+# Tryに対する意見
 class OpinionForTry(models.Model):
-    #id = models.IntegerField(unique=True)
     opinion = models.TextField()
     action = models.ForeignKey(TryAction, blank=True, null=True)
 
-
+# ミーティングクラス
 class Meeting(models.Model):
-    #id = models.CharField(unique=True, max_length=5)
     date = models.DateField()
     author = models.ForeignKey(Member, related_name='mtg_auther', blank=True, null=True)
     presenter = models.ForeignKey(Member, related_name='mtg_presenter', blank=True, null=True)
--- kaizen/views.py
+++ kaizen/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import ProblemKeep
 
 # Create your views here.
+
+# Tryを登録する
+def showtry(request) :
+    return render(request, '')
+
+def index(request):
+    pk_list = ProblemKeep.objects.order_by('title')
+    context = {'pk_list': pk_list, 'page_title': '課題リスト'}
+    return render(request, 'kaizen/index.html', context)
