8a5d6120067c5561b53cd9736ed720dffc7d46aa
acnaman <0B2012@prodgrp.iftc.co.jp>
2018-02-23 19:15:27 +0900

    add login/logout interface

--- kaizen/urls.py
+++ kaizen/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+    url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^$', views.index, name='index'),
 ]
--- kaizen/views.py
+++ kaizen/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import ProblemKeep
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -7,7 +8,9 @@ from .models import ProblemKeep
 def showtry(request) :
     return render(request, '')
 
+@login_required
 def index(request):
     pk_list = ProblemKeep.objects.order_by('title')
     context = {'pk_list': pk_list, 'page_title': '課題リスト'}
     return render(request, 'kaizen/index.html', context)
+    
